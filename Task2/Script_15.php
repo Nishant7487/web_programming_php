189164bd328d1c411ef748d2194e2f00bb05261f
Nishant7487 <127898307+Nishant7487@users.noreply.github.com>
2024-09-26 08:27:45 +0530

    Update Script_15.php

--- Task2/Script_15.php
+++ Task2/Script_15.php
@@ -1,1 +1,35 @@
 <!-- • Script_15: Write a PHP script to find the largest of three numbers entered by the user. -->
+<html>
+<head>
+    <title>Largest Number</title>
+</head>
+<body>
+    <form method="post">
+        Enter first number: <input type="number" name="no1" required><br><br>
+        Enter second number: <input type="number" name="no2" required><br><br>
+        Enter third number: <input type="number" name="no3" required><br><br>
+        <input type="submit" name="submit" value="Find Largest">
+    </form>
+
+    <?php
+    if ($_REQUEST['submit']) {
+        $no1 = $_REQUEST['num1'];
+        $no2 = $_REQUEST['num2'];
+        $no3 = $_REQUEST['num3'];
+
+        // Find the largest number
+      if($no1>$no2 && $no1>$no3){
+        $largest = $a;
+      }
+      elseif(&no2>&no1 && $no2>$no3){
+        $largest = $b;
+      }
+      else{
+        $largest = $c;
+      }
+
+      echo "<br>The largest number is: " . $largest;
+    }
+    ?>
+</body>
+</html>

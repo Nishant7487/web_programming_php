7980ac649f3189f353f77f0b6f5e19c38a6b6b2c
Nishant7487 <127898307+Nishant7487@users.noreply.github.com>
2024-09-26 08:09:04 +0530

    Update Script_12.php

--- Task2/Script_12.php
+++ Task2/Script_12.php
@@ -1,8 +1,8 @@
 <!-- • Script_12: Write a PHP script to concatenate two strings and display the combined result. -->
 
 <?php
-$string1 = "Hello, ";
-$string2 = "World!";
-$combinedString = $string1 . $string2;
-echo $combinedString;
+$string1 = "Atmiya ";
+$string2 = "University";
+$string3 = $string1 . $string2;
+echo "Combined Result of Two Concatenated Strings : " $string3;
 ?>

8f49080b4113c7e1b6eafdae6efcd13df4f7abe0
Nishant7487 <127898307+Nishant7487@users.noreply.github.com>
2024-09-26 08:42:56 +0530

    Update Script_17.php

--- Task2/Script_17.php
+++ Task2/Script_17.php
@@ -1,7 +1,7 @@
 <!-- • Script_17: Create a PHP script to check if a given year is a leap year and display the result. -->
 
 <?php
-$year = 2024;
+$year = 2220;
 if (($year % 4 == 0 && $year % 100 != 0) || ($year % 400 == 0)) {
     echo $year . " is a leap year.";
 } else {

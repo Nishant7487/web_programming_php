822f06c8947f614bfa71eaf380a3bce556663486
Nishant7487 <127898307+Nishant7487@users.noreply.github.com>
2024-09-26 08:11:51 +0530

    Update Script_12.php

--- Task2/Script_12.php
+++ Task2/Script_12.php
@@ -4,5 +4,5 @@
 $string1 = "Atmiya ";
 $string2 = "University";
 $string3 = $string1 . $string2;
-echo "Combined Result of Two Concatenated Strings : " $string3;
+echo "Combined Result of Two Concatenated Strings : ". $string3;
 ?>

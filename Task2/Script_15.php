b62afdac1dcda926e3a40ea9a18575694e60efad
Nishant7487 <127898307+Nishant7487@users.noreply.github.com>
2024-09-26 09:03:04 +0530

    Update Script_15.php

--- Task2/Script_15.php
+++ Task2/Script_15.php
@@ -18,10 +18,10 @@
         $no3 = $_REQUEST['num3'];
 
         // Find the largest number
-      if($no1>$no2 && $no1>$no3){
+      if($no1 > $no2 && $no1 > $no3){
         $largest = $no1;
       }
-      elseif(&no2>&no1 && $no2>$no3){
+      elseif( $no2 > $no1 && $no2 > $no3){
         $largest = $no2;
       }
       else{

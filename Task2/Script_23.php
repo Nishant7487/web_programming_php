5ad2bb0bd4c5857709a75669d74417574843b64f
Nishant7487 <127898307+Nishant7487@users.noreply.github.com>
2024-09-26 08:54:26 +0530

    Update Script_23.php

--- Task2/Script_23.php
+++ Task2/Script_23.php
@@ -1,9 +1,21 @@
 <!-- • Script_23 Script to determine the largest among three numbers and display the result. -->
-
 <?php
-$num1 = 10;
-$num2 = 20;
-$num3 = 15;
-$largest = max($num1, $num2, $num3);
-echo "The largest number is: " . $largest;
+  $no1 = 110;
+  $no2 = 15;
+  $no3 = 50;
+
+  // Find the largest number
+  if($no1>$no2 && $no1>$no3){
+    $largest = $no1;
+  }
+  elseif(&no2>&no1 && $no2>$no3){
+    $largest = $no2;
+  }
+  else{
+    $largest = $no3;
+  }
+echo "<br>The largest number is: " . $largest;
+}
 ?>
+</body>
+</html>
